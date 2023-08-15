50e88aabf4c4b67dd6d121cad4a0d2cca582e901
Rohaniscoder <109896047+Rohaniscoder@users.noreply.github.com>
2023-08-15 15:53:25 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -12,8 +12,7 @@ def is_enabled(value, default):
 
 class script(object):
     HOME_BUTTONURL_UPDATES = environ.get("HOME_BUTTONURL_UPDATES", 'https://clicksfly.com/ref/LazyDeveloperr')
-    START_TXT = environ.get("START_TXT", '''Hello {},
-Myself <a href=https://t.me/{}>{}</a>,\n\nTrust me ! I can't even imagine how super-fast i can drive your Database channel \n\nAre you ready for Long Drive Baby...🤪''')
+    START_TXT = environ.get("START_TXT", '''Hello {}, I'm <a href=https://t.me/{}>{}</a>,\n I'm a Movies / Series Bot. Just Enter Correct Spelling What You Want!!''')
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝙼𝚈 𝙷𝙴𝙻𝙿 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
     ABOUT_TXT = """⚜️ My Name: {}
